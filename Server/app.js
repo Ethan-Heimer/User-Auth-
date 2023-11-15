481d33db46ce61e94a6f90f020f9584392bc4844
Ethan-Heimer <111470266+Ethan-Heimer@users.noreply.github.com>
2023-11-15 13:45:22 -0700

    message

--- Server/app.js
+++ Server/app.js
@@ -1,2 +1,40 @@
 require("dotenv").config();
 
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require("cors");
+const app = express();
+const authRoute = require("./routes/AuthRoute");
+const cookieParser = require("cookie-parser");
+
+const {MONGO_URL, PORT} = process.env;
+
+mongoose.connect(MONGO_URL)
+.then((x) => {
+    console.log('connected to db');
+}).catch(err => {
+    console.error(`Error connecting to database: ${err}`);
+})
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow_Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT")
+    res.setHeader("Access-Controll-Allow-Headers", "Content-Type")
+})
+
+const corsOptions = {
+    origin: ["http://localhost:3000"],
+    allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept"],
+    credentials: true,
+    methods: ['GET', 'POST']
+}
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+app.use(cookieParser());
+app.use('/', authRoute);
+
+app.listen(PORT, () =>{
+    console.log(`Server is running on port ${PORT}`)
+})
--- Server/controllers/AuthController.js
+++ Server/controllers/AuthController.js
@@ -31,5 +31,23 @@ module.exports.Signup = async (req, res, next) => {
 }
 
 module.exports.Login = async (req, res, next) => {
-    
+   try{
+    const {email, password} = req.body;
+    if(!email || !password){
+        return res.json({message: "all fields requiered"})
+    }
+    const user = await User.findOne({email})
+    if (!user) {
+        return res.json({message: "Incorrect password are required"})
+    }
+    const token = createSecretToken(user._id);
+    res.cookie("token", token, {
+        withCredentials: true,
+        httpOnly: false
+    })
+    res.status(200).json({message: "User Logged in successfully", success: true});
+    next()
+   } catch (error) {
+    console.log(error);
+   }
 }
